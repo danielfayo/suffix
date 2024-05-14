3b281caf2faed0708b8ea0eb8062cfcb4852b599
danielfayo <daniel123olu@gmail.com>
2024-05-14 23:01:33 +0100

    feat: guess words

---
+++ lib/views/home/widget/guess_words.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:suffix/models/word.dart';
+import 'package:suffix/utils/colors.dart';
+import 'package:suffix/utils/text_styles.dart';
+import 'package:suffix/view_models/gameplay_viewmodel.dart';
+
+class GuessWords extends StatelessWidget {
+  const GuessWords({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<GameplayViewModel>(
+      builder: (context, gameplayViewModel, child) {
+        return Column(
+          children: gameplayViewModel.words
+              .map((word) => WordRow(word: word))
+              .toList(),
+        );
+      },
+    );
+  }
+}
+
+class WordRow extends StatelessWidget {
+  const WordRow({super.key, required this.word});
+
+  final Word word;
+
+  @override
+  Widget build(BuildContext context) {
+    double boxSize = (MediaQuery.of(context).size.width / 7).clamp(40, 47);
+
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: word.allLetters
+            .map((eachLetter) => Container(
+                  height: boxSize,
+                  width: boxSize,
+                  alignment: Alignment.center,
+                  margin: const EdgeInsets.only(right: 8),
+                  decoration: BoxDecoration(
+                    color: eachLetter.isInRightPosition
+                        ? kGreen
+                        : eachLetter.letterIsPresent
+                            ? kYellow
+                            : eachLetter.letterIsNotPresent
+                                ? kLight
+                                : kAccent,
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(width: 1, color: kDark),
+                  ),
+                  child: eachLetter.letter != null
+                      ? Text(
+                          eachLetter.letter!,
+                          style: kHeading,
+                        )
+                      : null,
+                ))
+            .toList(),
+      ),
+    );
+  }
+}
