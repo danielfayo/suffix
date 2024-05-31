aa32a2dcac792f415a9167553b11b0eeafa7bc0c
danielfayo <daniel123olu@gmail.com>
2024-05-31 14:59:06 +0100

    ui: invalid word snackbar

---
+++ lib/views/home/widget/invalid_word_snackbar.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:suffix/utils/colors.dart';
+import 'package:suffix/utils/text_styles.dart';
+
+void showInvalidWordSnackbar(BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(invalidWordSnackbar);
+}
+
+SnackBar invalidWordSnackbar = SnackBar(
+  content: IntrinsicWidth(
+    child: Container(
+      child: Text(
+        "Invalid Word",
+        style: kBodySm,
+        textAlign: TextAlign.center,
+      ),
+    ),
+  ),
+  margin: const EdgeInsets.only(bottom: 32, left: 32, right: 32),
+  behavior: SnackBarBehavior.floating,
+  elevation: 0,
+  backgroundColor: kLight,
+  shape: RoundedRectangleBorder(
+    side: const BorderSide(
+      color: kDark,
+      width: 1,
+    ),
+    borderRadius: BorderRadius.circular(24),
+  ),
+  duration: const Duration(seconds: 2),
+);
